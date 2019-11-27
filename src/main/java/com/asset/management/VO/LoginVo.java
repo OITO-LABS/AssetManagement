58ccd7edc5b188c9b149b424a82a99492d874fd0
gayathri-janardhanan <gayathritist@gmail.com>
2019-11-27 14:38:05 +0530

    login commit

---
+++ src/main/java/com/asset/management/VO/LoginVo.java
@@ -0,0 +1,13 @@
+package com.asset.management.VO;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class LoginVo {
+private String userName;
+private String password;
+}
