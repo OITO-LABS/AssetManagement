f38ef513eac33d34e17b6bd1f6a19da3b3c4bf73
Saranya Nair <saranyasasi1710@gmail.com>
2019-11-29 11:19:25 +0530

    no message

--- src/main/java/com/asset/management/VO/LoginVo.java
+++ src/main/java/com/asset/management/VO/LoginVo.java
@@ -1,5 +1,10 @@
 package com.asset.management.VO;
 
+import java.util.Date;
+
+import com.asset.management.dao.entity.Employee;
+import com.asset.management.dao.entity.Status;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,6 +13,11 @@ import lombok.ToString;
 @Getter
 @ToString
 public class LoginVo {
-private String userName;
-private String password;
+	private String userName;
+	private String password;
+	private Status status;
+	private Date loginTime;
+	private Date logoutTime;
+	private String role;
+	private Employee employee;
 }
