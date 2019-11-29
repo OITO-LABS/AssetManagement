1ac1dcbc133819d06cf2d2efb5364822875809f5
Saranya Nair <saranyasasi1710@gmail.com>
2019-11-29 12:39:56 +0530

    no message

--- src/main/java/com/asset/management/service/LoginService.java
+++ src/main/java/com/asset/management/service/LoginService.java
@@ -1,7 +1,13 @@
 package com.asset.management.service;
 
+import com.asset.management.VO.Mail;
+
 public interface LoginService {
 	void resetPassword();
 	void generatePasswordToken();
 	void validatePassword();
+	///
+	void sendmail(Mail obj);
+	String generatePasswordToken(Long empId);
+	void validatePassword(); 
 }
--- src/main/java/com/asset/management/service/LoginServiceImpl.java
+++ src/main/java/com/asset/management/service/LoginServiceImpl.java
@@ -1,22 +1,64 @@
 package com.asset.management.service;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
-public class LoginServiceImpl implements LoginService{
+import com.asset.management.VO.Mail;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.asset.management.dao.ReimbursementDaoImpl;
+
+@Component
+public class LoginServiceImpl implements LoginService {
+
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(LoginServiceImpl.class);
 	@Override
 	public void resetPassword() {
-		
+
 	}
 
-	@Override
-	public void generatePasswordToken() {
-		
+	public String generatePasswordToken(Long empId) {
+
+		try {
+
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] messageDigest = md.digest((empId.toString()).getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+            return hashtext;
+		}
+
+		catch (NoSuchAlgorithmException e) {  //
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void validatePassword() {
+
+	}
+	
+///
+	@Override
+	public void sendmail(Mail obj) {
+		 Mail mail = new Mail();
+	        mail.setTo(obj.getTo());
+	        mail.setSubject("OTP Mail");
+	        mail.setContent("Hereby,sending you an auto-generated mail from OITO-TRV Internal Project.To confirm your account, "
+	        		+ "please click here :https://www.google.com?"+obj.getToken());	
+	}
+
+	@Override
+	public void generatePasswordToken() {
+		// TODO Auto-generated method stub
 		
 	}
 
