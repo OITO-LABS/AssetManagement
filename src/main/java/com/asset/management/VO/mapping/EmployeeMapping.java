15187e988fba3fbc555529f8a14071f790d11102
gayathri-janardhanan <gayathritist@gmail.com>
2019-12-02 12:46:48 +0530

    emp

--- src/main/java/com/asset/management/VO/mapping/EmployeeMapping.java
+++ src/main/java/com/asset/management/VO/mapping/EmployeeMapping.java
@@ -10,9 +10,7 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 import org.springframework.data.domain.Page;
 
-import com.asset.management.VO.AssetVO;
 import com.asset.management.VO.EmployeeVo;
-import com.asset.management.dao.entity.AssetEntity;
 import com.asset.management.dao.entity.Employee;
 
 @Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
